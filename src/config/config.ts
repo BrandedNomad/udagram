0c86f58b1383fead1806f0b646224976c877d764
BrandedNomad <info@brandednomad.com>
2020-11-20 13:11:36 +0700

    initial commit

---
+++ src/config/config.ts
@@ -0,0 +1,19 @@
+export const config = {
+  "dev": {
+    "username": "udagramDev",
+    "password": "udagramDev",
+    "database": "postgres",
+    "host": "db-udagram-dev.cpivbzyu5d0c.ap-southeast-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "ap-southeast-1",
+    "aws_profile": "default",
+    "aws_media_bucket": "filestore-udagram-dev"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+};
